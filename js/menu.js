8ab9e1b256fcb05d0337d51463e4bdab0b2c37ed
innabatalova <batalovainna97@gmail.com>
2021-11-07 19:53:54 +0300

    Create: dimanic rendering cards

--- js/menu.js
+++ js/menu.js
@@ -1,18 +1,62 @@
+const cardsMenu = document.querySelector(".cards-menu");
+
+const cardInfo = document.querySelector(".card-info");
+
+const changeTitle = (restaurant) => {
+  const restaurantTitle = document.querySelector(".restaurant-title");
+  restaurantTitle.textContent = restaurant.name;
+};
+
 const renderItems = (data) => {
   //console.log(data);
 
-  data.forEach((elem) => {
-    console.log(elem);
+  //перебор массива с высодом каждого элемента массива в консоль
+  data.forEach(({ description, id, image, name, price }) => {
+    const card = document.createElement("card");
+
+    card.classList.add("card");
+
+    card.innerHTML = `
+    <img src="${image}" alt="${name}" class="card-image"/>
+      <div class="card-text">
+        <div class="card-heading">
+          <h3 class="card-title card-title-reg">${name}</h3>
+        </div>
+        <div class="card-info">
+          <div class="ingredients">${description}
+          </div>
+        </div>
+        <div class="card-buttons">
+          <button class="button button-primary button-add-cart">
+            <span class="button-card-text">В корзину</span>
+            <span class="button-cart-svg"></span>
+          </button>
+          <strong class="card-price-bold">${price} ₽</strong>
+        </div>
+      </div>        
+    `;
+
+    cardsMenu.append(card);
   });
 };
 
-const restourant = "tanuki";
+//превращение данные из json и обратно (через сервер можно менять значения атрибутов)
 
-fetch(`./db/${restourant}.json`)
-  .then((response) => response.json())
-  .then((data) => {
-    renderItems(data);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+//проверка наличия ключа в localStorage и выполнение кода, если ключ есть
+if (localStorage.getItem("restaurant")) {
+  const restaurant = JSON.parse(localStorage.getItem("restaurant"));
+  //console.log(restaurant.products);
+
+  changeTitle(restaurant);
+
+  fetch(`./db/${restaurant.products}`) //добавление текущего значения переменной в {}
+    .then((response) => response.json())
+    .then((data) => {
+      renderItems(data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+} else {
+  window.location.href = "/";
+}
